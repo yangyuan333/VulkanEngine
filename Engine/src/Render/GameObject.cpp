669c3169eb309e4dec7f8a773d9a22e9600c76b9
yangyuan333 <347065418@qq.com>
2024-10-08 16:26:50 +0800

    Add GameObject Material完善

--- Engine/src/Render/GameObject.cpp
+++ Engine/src/Render/GameObject.cpp
@@ -19,20 +19,27 @@ namespace VulkanEngine
 	void MeshComponent::CreateMaterialTexture(std::shared_ptr<ModelData::Material> material)
 	{
 		// Albedo
-		if (material->AlbedoTexture.Width == 1 && material->AlbedoTexture.Height == 1)
-		{
-			
-		}
+		std::shared_ptr<Image> albedoTexture = std::make_shared<Image>();
+		FillImage(
+			*albedoTexture, material->AlbedoTexture, 
+			VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, ImageOptions::MIPMAPS);
+		m_MaterialResource["albedo"] = albedoTexture;
 		// Normal
-		if (material->AlbedoTexture.Width == 1 && material->AlbedoTexture.Height == 1)
-		{
-
-		}
+		std::shared_ptr<Image> normalTexture = std::make_shared<Image>();
+		FillImage(
+			*normalTexture, material->NormalTexture,
+			VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, ImageOptions::MIPMAPS);
+		m_MaterialResource["normal"] = normalTexture;
 		// MetallicRoughness
-		if (material->AlbedoTexture.Width == 1 && material->AlbedoTexture.Height == 1)
-		{
-			
-		}
+		std::shared_ptr<Image> metallicRoughnessTexture = std::make_shared<Image>();
+		FillImage(
+			*metallicRoughnessTexture, material->MetallicRoughness,
+			VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
+			VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, ImageOptions::MIPMAPS);
+		m_MaterialResource["metallicRoughness"] = metallicRoughnessTexture;
+
 	}
 
 	MeshComponent::MeshComponent(std::vector<Vertex> vertices, std::vector<uint32_t> indices, std::shared_ptr<ModelData::Material> material)
